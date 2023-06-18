d8d66438c641f7153b3171a42b16e252a904f974
Ashik Muhammed <84127769+JinnSulthan@users.noreply.github.com>
2023-06-18 10:24:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import asyncio
-
 from pytgcalls import idle
-
 from config import call_py
-
 
 async def main():
     await call_py.start()
     print(
         """
-    ------------------
-   | EvaMaria Started! |
-    ------------------
+    -------------------
+   | Fathima Started! |
+    -------------------
 """
     )
     await idle()
-
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
