0bd72fa7ef213c4c2b517f89886066e12050d506
Ashik Muhammed <84127769+JinnSulthan@users.noreply.github.com>
2023-06-18 09:55:10 +0530

    👀

--- EvaMaria/admin.py
+++ EvaMaria/admin.py
@@ -9,18 +9,18 @@ from EvaMaria.helpers.queues import QUEUE, clear_queue
 
 @Client.on_message(filters.command(["skip"], prefixes=f"{HNDLR}"))
 @authorized_users_only
-async def skip(client, m: Message):
+async def skip_command(client, m: Message):
     await m.delete()
     chat_id = m.chat.id
     if len(m.command) < 2:
         op = await skip_current_song(chat_id)
         if op == 0:
-            await m.reply("**🙄There's nothing in the queue to skip!**")
+            await m.reply_text("**🙄 There's nothing in the queue to skip!**")
         elif op == 1:
-            await m.reply("**😩Empty Queue, Leaving Voice Chat**")
+            await m.reply_text("**😩 Empty Queue, Leaving Voice Chat**")
         else:
-            await m.reply(
-                f"**⏭ Skipped** \n**🎧 Now playing** - [{op[0]}]({op[1]}) | `{op[2]}`",
+            await m.reply_text(
+                f"**⏭ Skipped**\n**🎧 Now playing** - [{op[0]}]({op[1]}) | `{op[2]}`",
                 disable_web_page_preview=True,
             )
     else:
@@ -38,54 +38,54 @@ async def skip(client, m: Message):
                         pass
                     else:
                         OP = OP + "\n" + f"**#⃣{x}** - {hm}"
-            await m.reply(OP)
+            await m.reply_text(OP)
 
 
 @Client.on_message(filters.command(["end", "stop"], prefixes=f"{HNDLR}"))
 @authorized_users_only
-async def stop(client, m: Message):
+async def stop_command(client, m: Message):
     await m.delete()
     chat_id = m.chat.id
     if chat_id in QUEUE:
         try:
             await call_py.leave_group_call(chat_id)
             clear_queue(chat_id)
-            await m.reply("**😐End**")
+            await m.reply_text("**😐 End**")
         except Exception as e:
-            await m.reply(f"**ERROR** \n`{e}`")
+            await m.reply_text(f"**ERROR**\n`{e}`")
     else:
-        await m.reply("**🤨Nothing is playing !**")
+        await m.reply_text("**🤨 Nothing is playing!**")
 
 
 @Client.on_message(filters.command(["pause"], prefixes=f"{HNDLR}"))
 @authorized_users_only
-async def pause(client, m: Message):
+async def pause_command(client, m: Message):
     await m.delete()
     chat_id = m.chat.id
     if chat_id in QUEUE:
         try:
             await call_py.pause_stream(chat_id)
-            await m.reply(
+            await m.reply_text(
                 f"**⏸ Paused.**\n\n• To resume playback, use the command » {HNDLR}resume"
             )
         except Exception as e:
-            await m.reply(f"**ERROR** \n`{e}`")
+            await m.reply_text(f"**ERROR**\n`{e}`")
     else:
-        await m.reply("**🤨Nothing is playing!**")
+        await m.reply_text("**🤨 Nothing is playing!**")
 
 
 @Client.on_message(filters.command(["resume"], prefixes=f"{HNDLR}"))
 @authorized_users_only
-async def resume(client, m: Message):
+async def resume_command(client, m: Message):
     await m.delete()
     chat_id = m.chat.id
     if chat_id in QUEUE:
         try:
             await call_py.resume_stream(chat_id)
-            await m.reply(
-                f"**▶ Resumed**\n\n• To pause playback, use the command » {HNDLR}pause**"
+            await m.reply_text(
+                f"**▶ Resumed**\n\n• To pause playback, use the command » {HNDLR}pause"
             )
         except Exception as e:
-            await m.reply(f"**ERROR** \n`{e}`")
+            await m.reply_text(f"**ERROR**\n`{e}`")
     else:
-        await m.reply("**🙄 Nothing is currently paused!**")
+        await m.reply_text("**🙄 Nothing is currently paused!**")
