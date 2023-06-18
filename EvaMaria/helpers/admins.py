6711cb7e4ee949126def3f2ad3d191b5cdfafa63
Ashik Muhammed <84127769+JinnSulthan@users.noreply.github.com>
2023-06-18 10:11:45 +0530

    Update admins.py

--- EvaMaria/helpers/admins.py
+++ EvaMaria/helpers/admins.py
@@ -1,16 +1,13 @@
 from typing import List
-
 from pyrogram.types import Chat
-
 from EvaMaria.helpers.get_admins import get as gett
 from EvaMaria.helpers.get_admins import set
 
-
 async def get_administrators(chat: Chat) -> List[int]:
-    get = gett(chat.id)
+    admins = gett(chat.id)
 
-    if get:
-        return get
+    if admins:
+        return admins
     else:
         administrators = await chat.get_members(filter="administrators")
         to_set = []
