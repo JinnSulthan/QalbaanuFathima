7e147287fd61f8fbb22dba7b9bf23fbaeb8da293
Ashik Muhammed <84127769+JinnSulthan@users.noreply.github.com>
2024-03-12 20:52:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import asyncio
+from pyrogram import Client
 from pytgcalls import idle
 from config import call_py
 
 async def main():
-    await call_py.start()
-    print(
-        """
-    -------------------
-   | Fathima Started! |
-    -------------------
-"""
-    )
-    await idle()
+    async with Client("my_account") as app:
+        await call_py.start(app)
+        print(
+            """
+        -------------------
+       | Fathima Started! |
+        -------------------
+    """
+        )
+        await idle()
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+if __name__ == '__main__':
+    import asyncio
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
