bfa822d25f7723405d0407e699d5357c92c36795
Ashik Muhammed <84127769+JinnSulthan@users.noreply.github.com>
2024-03-12 20:54:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-
 from dotenv import load_dotenv
 from pyrogram import Client, filters
 from pytgcalls import PyTgCalls
@@ -15,11 +14,16 @@ SESSION = os.getenv("SESSION")
 HNDLR = os.getenv("HNDLR", "/")
 SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
 
+# Create PyTgCalls instance
+call_py = PyTgCalls()
 
+# Create Pyrogram Client
+app = Client(SESSION, API_ID, API_HASH)
+
+# Apply filters
 contact_filter = filters.create(
-    lambda _, __, message: (message.from_user and message.from_user.is_contact)
-    or message.outgoing
+    lambda _, __, message: (message.from_user and message.from_user.is_contact) or message.outgoing
 )
 
-bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="EvaMaria"))
-call_py = PyTgCalls(bot)
+# Run the bot
+app.run()
